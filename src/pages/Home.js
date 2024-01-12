2ca40a8e80aabc027c9e778b8a0c5eb58f2985e1
Benedetto Marino <benedetto.marino20495@gmail.com>
2024-01-12 02:09:41 +0100

    bestemmie

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,10 @@ import Header from "../pageComponents/header";
 const Home = () => {
   return <>
     <Header />
+    <div className="discover" >
+      Discover the world <br/>
+      of plants with us
+    </div>
     <Footer /></>;
 };
 
