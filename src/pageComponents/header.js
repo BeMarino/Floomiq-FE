46be2418511dc223ef6f8bc89db01b940d55762f
Benedetto Marino <benedetto.marino20495@gmail.com>
2024-01-13 15:46:25 +0100

    update home

--- src/pageComponents/header.js
+++ src/pageComponents/header.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import Nav from './Nav';
 import Hamburger from './Hamburger';
-import Link from 'react-router-dom';
 import DropDownRouter from './DropDownRouter';
 
 function Header() {
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,8 +5,26 @@ const Home = () => {
   return <>
     <Header />
     <div className="discover" >
-      Discover the world <br/>
-      of plants with us
+      <div className="bg-text">
+        <h1>Discover the world <br />
+          of plants with us</h1>
+        <h5 style={{ fontWeight: "lighter" }}>Explore our extensive plant database and find the perfect plants
+          <br />for your garder. With over 30.000 plants to choosefrom, you'll have all
+          <br />the information you need to create beautiful and thriving garder.
+        </h5>
+      </div>
+    </div>
+    <div className="discover-row">
+      <div>
+        <h1>Discover the world <br />
+          of plants with us</h1>
+        <h5 style={{ fontWeight: "lighter" }}>Explore our extensive plant database and find the perfect plants
+          <br />for your garder. With over 30.000 plants to choosefrom, you'll have all
+          <br />the information you need to create beautiful and thriving garder.
+        </h5>
+      </div>
+
+      <div>image</div>
     </div>
     <Footer /></>;
 };
