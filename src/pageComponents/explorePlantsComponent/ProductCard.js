8b782447380892234debf5f48682f3c8d94a8ac6
Benedetto Marino <bennym20495@gmail.com>
2024-03-13 22:19:58 +0100

    add accordion in product card

--- src/pageComponents/explorePlantsComponent/ProductCard.js
+++ src/pageComponents/explorePlantsComponent/ProductCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { RxWidth } from "react-icons/rx";
 import { GiTreeGrowth, GiSandsOfTime } from "react-icons/gi";
 import { GoPlus } from "react-icons/go";
@@ -7,19 +7,23 @@ import { MdHeight } from "react-icons/md";
 import { LiaThermometerHalfSolid } from "react-icons/lia";
 import { GiSolidLeaf } from "react-icons/gi";
 
-
-
-
 import { CiHeart } from "react-icons/ci";
 
 
 export default function ProductCard({ product, addItemToCart, toggleCart, openSideCart }) {
+
+    const [accordionOpen, setVisible] = useState(false);
+
     function addProduct() {
 
         if (openSideCart == false)
             toggleCart();
 
         addItemToCart(product);
+    }
+
+    function toggleAccordion(e){
+        setVisible(!accordionOpen);
     }
 
     let imageUrl = "url(" + product.image + ")";
@@ -31,7 +35,21 @@ export default function ProductCard({ product, addItemToCart, toggleCart, openSi
                         <button className="addToFav" onClick={addProduct}><CiHeart /></button>
                     </div>
                     <button className="addToCart" onClick={addProduct}><GoPlus /></button>
-                    <div className="plantName">{product.name}</div>
+                    <div id="accordion-flush" data-accordion="collapse" data-active-classes="bg-white dark:bg-gray-900 text-gray-900 dark:text-white" data-inactive-classes="text-gray-500 dark:text-gray-400">
+                        <h2 id={"accordion-flush-heading"}>
+                            <button onClick={toggleAccordion} type="button" className="plantName flex items-center justify-between  w-11/12 rtl:text-right text-gray-500 dark:text-gray-400" aria-expanded="true" >
+                                <span>{product.name}</span>
+                                <svg data-accordion-icon className={accordionOpen?"w-3 h-3 shrink-0" :"w-3 h-3 rotate-180 shrink-0"} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5" />
+                                </svg>
+                            </button>
+                        </h2>
+                        <div className={accordionOpen ? "":"hidden"} aria-labelledby={"accordion-flush-heading"}>
+                            <div className="py-2 border-b border-gray-200 dark:border-gray-700">
+                                <p className="mx-2 text-xs text-left text-gray-500 dark:text-gray-400">L’acero è un albero presente in molte varietà, alcune caratterizzate da spettacolari chiome rosse che le rendono particolarmente apprezzate a scopo ornamentale. Si tratta di un albero splendido, rustico e resistente, semplice da coltivare in giardino e in vaso.</p>
+                            </div>
+                        </div>
+                    </div>
                     <div className="details" id="details1">
                         {/* <div className="detail"><p className="type"><img src={indoor_outdoor} title="Environment" /></p>
                                 <p className="description" >Outdoor</p>
